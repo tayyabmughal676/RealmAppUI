c150ff88a3b3def29e81ff9696d8504d1df26694
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-09-01 17:10:57 +0500

    Initial Commit

---
+++ RealmAppUI/RealmAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmAppUIApp.swift
+//  RealmAppUI
+//
+//  Created by Thor on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
